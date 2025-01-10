fc0e025d92bc8b27ca41bbdad5bbdb0b26d3a4b4
itaysegev1 <itaysegev1234@gmail.com>
2025-01-10 18:52:24 +0200

    adding another letters group

--- Main.py
+++ Main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     print(f"{BLUE}Accurate on train {accuracy:.2%}{RESET}")
 
     # Checking the results for all the letters and print
-    print(f"{GREEN}\nResults for all the letters on the train: {RESET}")
+    print(f"{GREEN}Results for all the letters on the train: {RESET}")
     for i, input_vector in enumerate(inputs):
         prediction = net.predict(input_vector)
         if np.array_equal(prediction, [1, 0, 0]):
@@ -43,13 +43,13 @@ if __name__ == '__main__':
         expected = "A-I" if i <= 8 else "J-R" if i <= 17 else "S-Z"
         color = BLUE if ans == expected else RED
 
-        print(f"{color}The letter {chr(65 + i)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+        print(f"\t{color}The letter {chr(65 + i)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
 
-    for i in range(5, 16, 5):
+    for i in range(5, 21, 5):
         vecs = VectorsFactory.create_vectors_with_mistakes(inputs, percent=i / 100)
         accuracy = net.calculate_accuracy(vecs, outputs)
         print(f"\n{PURPLE}the accuracy with {i}% of mistake is: {accuracy:.2%}{RESET}")
-        print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
+        print(f"{GREEN}Results for this group: {RESET}")
         for j, input_vector in enumerate(vecs):
             prediction = net.predict(input_vector)
             if np.array_equal(prediction, [1, 0, 0]):
@@ -61,12 +61,12 @@ if __name__ == '__main__':
 
             expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
             color = BLUE if ans == expected else RED
-            print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+            print(f"\t{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
 
     test2vecs = VectorsFactory.create_bold_letters_vector()
     accuracy = net.calculate_accuracy(test2vecs, outputs)
     print(f"\n{PURPLE}the accuracy with group number 1: {accuracy:.2%}{RESET}")
-    print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
+    print(f"{GREEN}Results for this group: {RESET}")
     for j, input_vector in enumerate(test2vecs):
         prediction = net.predict(input_vector)
         if np.array_equal(prediction, [1, 0, 0]):
@@ -78,12 +78,12 @@ if __name__ == '__main__':
 
         expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
         color = BLUE if ans == expected else RED
-        print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+        print(f"\t{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
 
     test3vecs = VectorsFactory.create_letters_v3_vector()
     accuracy = net.calculate_accuracy(test3vecs, outputs)
     print(f"\n{PURPLE}the accuracy with group number 2: {accuracy:.2%}{RESET}")
-    print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
+    print(f"{GREEN}Results for this group: {RESET}")
     for j, input_vector in enumerate(test3vecs):
         prediction = net.predict(input_vector)
         if np.array_equal(prediction, [1, 0, 0]):
@@ -95,12 +95,12 @@ if __name__ == '__main__':
 
         expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
         color = BLUE if ans == expected else RED
-        print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+        print(f"\t{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
 
     test4vecs = VectorsFactory.create_letters_v4_vector()
     accuracy = net.calculate_accuracy(test4vecs, outputs)
     print(f"\n{PURPLE}the accuracy with group number 3: {accuracy:.2%}{RESET}")
-    print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
+    print(f"{GREEN}Results for this group: {RESET}")
     for j, input_vector in enumerate(test4vecs):
         prediction = net.predict(input_vector)
         if np.array_equal(prediction, [1, 0, 0]):
@@ -112,4 +112,4 @@ if __name__ == '__main__':
 
         expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
         color = BLUE if ans == expected else RED
-        print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+        print(f"\t{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
