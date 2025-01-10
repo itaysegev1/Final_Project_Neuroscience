8c8d5ccb53ffc64060cbbc114b29368d151d0b1b
itaysegev1 <itaysegev1234@gmail.com>
2025-01-10 18:09:44 +0200

    adding another letters group

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import numpy as np
 RED = "\033[91m"
 GREEN = "\033[92m"
 BLUE = "\033[94m"
+PURPLE = "\033[95m"
 RESET = "\033[0m"
 
 from HebbianNetwork import HebbianNetwork
@@ -29,7 +30,7 @@ if __name__ == '__main__':
     print(f"{BLUE}Accurate on train {accuracy:.2%}{RESET}")
 
     # Checking the results for all the letters and print
-    print(f"{GREEN}\nResults for all the letters: {RESET}")
+    print(f"{GREEN}\nResults for all the letters on the train: {RESET}")
     for i, input_vector in enumerate(inputs):
         prediction = net.predict(input_vector)
         if np.array_equal(prediction, [1, 0, 0]):
@@ -43,3 +44,74 @@ if __name__ == '__main__':
         color = BLUE if ans == expected else RED
 
         print(f"{color}The letter {chr(65 + i)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+
+    for i in range (0,16,5):
+        vecs=VectorsFactory.create_vectors_with_mistakes(inputs,percent=i/100)
+        accuracy = net.calculate_accuracy(vecs, outputs)
+        print(f"\n{PURPLE}the accuracy with {i}% of mistake is: {accuracy:.2%}{RESET}")
+        print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
+        for j, input_vector in enumerate(vecs):
+            prediction = net.predict(input_vector)
+            if np.array_equal(prediction, [1, 0, 0]):
+                ans = "A-I"
+            elif np.array_equal(prediction, [0, 1, 0]):
+                ans = "J-R"
+            else:
+                ans = "S-Z"
+
+            expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
+            color = BLUE if ans == expected else RED
+            print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+
+    test2vecs = VectorsFactory.create_bold_letters_vector()
+    accuracy = net.calculate_accuracy(test2vecs, outputs)
+    print(f"\n{PURPLE}the accuracy with group number 1: {accuracy:.2%}{RESET}")
+    print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
+    for j, input_vector in enumerate(test2vecs):
+        prediction = net.predict(input_vector)
+        if np.array_equal(prediction, [1, 0, 0]):
+            ans = "A-I"
+        elif np.array_equal(prediction, [0, 1, 0]):
+            ans = "J-R"
+        else:
+            ans = "S-Z"
+
+        expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
+        color = BLUE if ans == expected else RED
+        print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+
+    test3vecs = VectorsFactory.create_letters_v3_vector()
+    accuracy = net.calculate_accuracy(test3vecs, outputs)
+    print(f"\n{PURPLE}the accuracy with group number 2: {accuracy:.2%}{RESET}")
+    print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
+    for j, input_vector in enumerate(test3vecs):
+        prediction = net.predict(input_vector)
+        if np.array_equal(prediction, [1, 0, 0]):
+            ans = "A-I"
+        elif np.array_equal(prediction, [0, 1, 0]):
+            ans = "J-R"
+        else:
+            ans = "S-Z"
+
+        expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
+        color = BLUE if ans == expected else RED
+        print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+
+    test4vecs = VectorsFactory.create_letters_v4_vector()
+    accuracy = net.calculate_accuracy(test4vecs, outputs)
+    print(f"\n{PURPLE}the accuracy with group number 3: {accuracy:.2%}{RESET}")
+    print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
+    for j, input_vector in enumerate(test4vecs):
+        prediction = net.predict(input_vector)
+        if np.array_equal(prediction, [1, 0, 0]):
+            ans = "A-I"
+        elif np.array_equal(prediction, [0, 1, 0]):
+            ans = "J-R"
+        else:
+            ans = "S-Z"
+
+        expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
+        color = BLUE if ans == expected else RED
+        print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
+
+
