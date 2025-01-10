614b63d3af0ed6d5d0deff88043097606fd15d0d
itaysegev1 <itaysegev1234@gmail.com>
2025-01-10 18:22:47 +0200

    adding another letters group

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-
 import numpy as np
+
 RED = "\033[91m"
 GREEN = "\033[92m"
 BLUE = "\033[94m"
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     errors = net.train(
         inputs,
         outputs,
-        epochs=500
+        epochs=5000
     )
 
     # evaluate the network
@@ -45,8 +45,8 @@ if __name__ == '__main__':
 
         print(f"{color}The letter {chr(65 + i)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
 
-    for i in range (0,16,5):
-        vecs=VectorsFactory.create_vectors_with_mistakes(inputs,percent=i/100)
+    for i in range(5, 16, 5):
+        vecs = VectorsFactory.create_vectors_with_mistakes(inputs, percent=i / 100)
         accuracy = net.calculate_accuracy(vecs, outputs)
         print(f"\n{PURPLE}the accuracy with {i}% of mistake is: {accuracy:.2%}{RESET}")
         print(f"{GREEN}\nResults for all the letters on the train: {RESET}\n")
@@ -113,5 +113,3 @@ if __name__ == '__main__':
         expected = "A-I" if j <= 8 else "J-R" if j <= 17 else "S-Z"
         color = BLUE if ans == expected else RED
         print(f"{color}The letter {chr(65 + j)} Suppose to be in: {expected},and the answer is: {ans}{RESET}")
-
-
